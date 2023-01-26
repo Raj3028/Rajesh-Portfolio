4865629adb2fe55e7b85542fd7c00923a52dee9f
RAJESH KUMAR SAHOO <rajeshkumarsahoo006@gmail.com>
2023-01-26 21:47:44 +0530

    PortFolio

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import Particle from "../Particle";
+import "./Contact.css"
 import {
   AiFillGithub,
   AiOutlineTwitter,
@@ -13,9 +14,49 @@ import EmailIcon from "@mui/icons-material/Email";
 import PlaceIcon from "@mui/icons-material/Place";
 
 function Contact() {
+
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [phone, setPhone] = useState('')
+  const [message, setMessage] = useState('')
+
+  const submitHandler = (event) => {
+    event.preventDefault()
+
+    const config = {
+      SecureToken: "a4587958-816e-4932-92d7-edc5eea168fb",
+      To: 'rajeshkumarsahoo006@gmail.com',
+      From: email,
+      Subject: "This is the form my PortFolio!",
+      Body: "Name:" + name
+        + "<br> Email:" + email
+        + "<br> Phone No:" + phone
+        + "<br> Message:" + message
+
+      // `${name} connected to you over email. And the Email is '${email} \n Phone: ${phone} \n Body: ${message}'`
+    }
+
+    if (window.Email) {
+      window.Email.send(config).then((message) => alert(message)) //SmtpJS.cm
+    }
+  }
+
   return (
     <section>
       <Container fluid className="contact-section">
+
+        <Container className="container1">
+          <form onSubmit={submitHandler}>
+            <h3>Get In Touch</h3>
+            <input type="text" id="name" placeholder="Your Name" required value={name} onChange={(e) => setName(e.target.value)} />
+            <input type="email" id="email" placeholder="Email id" required value={email} onChange={(e) => setEmail(e.target.value)} />
+            <input type="text" id="phone" placeholder="Phone no." required value={phone} onChange={(e) => setPhone(e.target.value)} />
+            <textarea id="message" rows="4" placeholder="Text me.." value={message} onChange={(e) => setMessage(e.target.value)}></textarea>
+            <button type="submit"> Send</button>
+          </form>
+        </Container>
+
+
         <Container>
           <Particle />
           <Row>
